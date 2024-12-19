27c1af2b7e250b19ed4e435d6f455c1c730e378f
pieterjansen8 <pieterjansen8@icloud.com>
2024-12-19 23:59:41 +0100

    FIXED: eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { rule } from "postcss/lib/postcss";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,8 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  rule("@typescript-eslint/no-unused-vars", "off"),
+  rule("@typescript-eslint/no-non-null-asserted-optional-chain", "off"),
 ];
 
 export default eslintConfig;
