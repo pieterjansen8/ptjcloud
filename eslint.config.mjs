fcf8f1f1ed99744d63c209bf966d5807575c7e64
pieterjansen8 <pieterjansen8@icloud.com>
2024-12-19 23:39:07 +0100

    FIXED: eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import { rule } from "postcss/lib/postcss";
+const { rule } = pkg
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
