e2aeccde32810adcf7f650ccc09c7689bea2f92f
pieterjansen8 <pieterjansen8@icloud.com>
2025-02-23 17:26:48 +0100

    fixed eslint errors ):

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.config(
     {
       extends: ["next/core-web-vitals", "next/typescript"],
-      config:{ 
+      rules: {
         "@typescript-eslint/no-explicit-any": "off"
       }
     }
